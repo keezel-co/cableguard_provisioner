8b0856fbc8e0d356d3b8aceb7aa5aaa2a8953bf1
Menso Heus <menso@keezel.co>
2019-09-03 14:16:40 +0200

    Change name from Cableguard to Portunus due to trademark issues

--- README.md
+++ README.md
@@ -1,31 +1,31 @@
-Cableguard Provisioner      
+Portunus Provisioner      
 ===========================
 
-This Ansible script will install Wireguard on a server and register that server with the Cableguard API for automatic provisioning.
+This Ansible script will install Wireguard on a server and register that server with the Portunus API for automatic provisioning.
 
 Requirements
 ------------
 
 Ansible >= 2.8
-Cableguard API instance
-Server token from Cableguard API
+Portunus API instance
+Server token from Portunus API
 
 New server installer
 --------------------
 
-This script will help you to provision a new server. To do that you simply add your server to your Cableguard API instance, get the `server_token` after registration and use them as parameters when executing the script:
+This script will help you to provision a new server. To do that you simply add your server to your Portunus API instance, get the `server_token` after registration and use them as parameters when executing the script:
 
 ```
 Usage: ./deploy.sh <params> where params can be:
        -h To display this help and exit
        -t Token to use
-       -w Cableguard host/ip from where configuration is downloaded
+       -w Portunus host/ip from where configuration is downloaded
        -s Host/ip of Wireguard server to provision
   Optional:
-       -p Cableguard port (default 2443)
+       -p Portunus port (default 2443)
        -u Remote user of wireguard's SSH (root or sudoable user) (default root)
 
-Example: ./deploy.sh -t 5feb9792-add8-4d1c-8737-23b6313b4c8f -h cableguard.example.com -w wireguard.example.com -u ubuntu
+Example: ./deploy.sh -t 5feb9792-add8-4d1c-8737-23b6313b4c8f -h portunus.example.com -w wireguard.example.com -u ubuntu
 ```
 
 Testing with Vagrant
--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ show_help()
   echo "Usage: $0 <params> where params can be:"
   echo "       -h To display this help and exit"
   echo "       -t Token to use"
-  echo "       -w Cableguard host/ip from where configuration is downloaded"
+  echo "       -w portunus host/ip from where configuration is downloaded"
   echo "       -s Host/ip of Wireguard server to provision"
   echo "  Optional:"
-  echo "       -p Cableguard port (default 2443)"
+  echo "       -p portunus port (default 2443)"
   echo "       -u Remote user of wireguard's SSH (root or sudoable user) (default root)"
   echo -e " "
-  echo "Example: $0 -t 5feb9792-add8-4d1c-8737-23b6313b4c8f -w cableguard.example.com -s wireguard.example.com -u ubuntu"
+  echo "Example: $0 -t 5feb9792-add8-4d1c-8737-23b6313b4c8f -w portunus.example.com -s wireguard.example.com -u ubuntu"
   echo -e " "
 }
 
@@ -28,13 +28,13 @@ do
       export JOIN_TOKEN="${OPTARG}"
       ;;
     w)
-      export CABLEGUARD_SERVER="${OPTARG}"
+      export PORTUNUS_SERVER="${OPTARG}"
       ;;
     s)
       export WIREGUARD_TARGET="${OPTARG}"
       ;;
     p)
-      export CABLEGUARD_PORT="${OPTARG}"
+      export PORTUNUS_PORT="${OPTARG}"
       ;;
     u)
       export REMOTE_USER="${OPTARG}"
@@ -42,7 +42,7 @@ do
   esac
 done
 
-if [ -z "$JOIN_TOKEN" ] || [ -z "$WIREGUARD_TARGET" ] || [ -z "$CABLEGUARD_SERVER" ]
+if [ -z "$JOIN_TOKEN" ] || [ -z "$WIREGUARD_TARGET" ] || [ -z "$PORTUNUS_SERVER" ]
 then
   echo "Missing parameters"
   show_help
@@ -54,11 +54,11 @@ then
   REMOTE_USER='ubuntu'
 fi
 
-if [ -z "$CABLEGUARD_PORT" ]
+if [ -z "$PORTUNUS_PORT" ]
 then
-  export CABLEGUARD_PORT='2443'
+  export PORTUNUS_PORT='2443'
 else
-  export CABLEGUARD_PORT="$WGPT_PORT"
+  export PORTUNUS_PORT="$WGPT_PORT"
 fi
 
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i "${WIREGUARD_TARGET}," -u "${REMOTE_USER}" wgpt.yml
